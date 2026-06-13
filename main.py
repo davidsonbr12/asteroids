b6b2c75b5926215514e818567abc44a8ecf0eaf1
briandavidson <davidsonbr12@gmail.com>
2026-06-12 23:17:06 -0700

    fixed dt

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
-def main():
+
+
+def main() -> None:
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
             log_state()
-            for event in pygame.event.get():
-                pass
-            screen.fill("black")
-            pygame.display.flip()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
+            
+            screen.fill("black")
+            pygame.display.flip()
+
+            dt = clock.tick(60) / 1000
     print(f"Starting Asteroids with pygame version:s {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
